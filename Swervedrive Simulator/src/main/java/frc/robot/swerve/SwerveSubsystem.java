01163e8818464512e72b86f747a403cc00636e6b
Rohit Vankamamidi <rohitvankamamidi864@gmail.com>
2026-05-21 09:07:35 -0400

    made a swerve module class

--- Swervedrive Simulator/src/main/java/frc/robot/swerve/SwerveSubsystem.java
+++ Swervedrive Simulator/src/main/java/frc/robot/swerve/SwerveSubsystem.java
@@ -3,11 +3,17 @@ package frc.robot.swerve;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveSubsystem extends SubsystemBase{
+    
     private DriveStates currentState = DriveStates.IDLE;
     
     public enum DriveStates {
         IDLE, 
         FIELD_ORIENTED, 
         ROBOT_ORIENTED
+    }
+
+    @Override
+    public void periodic() {
+        
     }
 }
