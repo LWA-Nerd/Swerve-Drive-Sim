5f2dec0f93670f00856c1a62d66050db7d6c3fc5
Rohit Vankamamidi <rohitvankamamidi864@gmail.com>
2026-05-21 21:58:47 -0400

    made instance variables for the swerve module class

---
+++ Swervedrive Simulator/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class Constants {
+
+    public static class DriveConstants {
+            public static final double driveP = 0;
+            public static final double driveI = 0;
+            public static final double driveD = 0;
+
+            public static final double turnP = 0;
+            public static final double turnI = 0;
+            public static final double turnD = 0;
+    }
+}
